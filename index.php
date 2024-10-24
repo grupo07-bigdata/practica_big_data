29dcdfba034e49b2802d50aac2060636a6858b5f
Ludmila Marques <l.marques@bestplant.com>
2024-10-24 20:20:20 +0200

    Pagina web creada

---
+++ index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Web Javier Cerejido Cortés</title>
+    <link rel="stylesheet" href="estilos.css">
+    <link rel="apple-touch-icon" sizes="180x180" href="logos/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="logos/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="logos/favicon-16x16.png">
+</head>
+<body>
+
+    <div class="main-container">
+        <!-- Columna izquierda: Acerca de mí y Contacto -->
+        <div class="left-column">
+            <!-- Acerca de mí -->
+            <div class="section" id="about">
+                <h2>Acerca de mí</h2>
+                <p>Escribe una breve descripción sobre ti.</p>
+            </div>
+
+            <!-- Contacto -->
+            <div class="section" id="contact">
+                <h2>Contacto</h2>
+                <p>📧 ivajcc@hotmail.com</p>
+                <p>ivajcc@hotmail.com</p>
+            </div>
+        </div>
+
+        <!-- Proyectos (lado derecho) -->
+        <div class="section" id="projects">
+            <h2>Proyectos</h2>
+            
+            <!-- Div que contiene los botones de proyectos -->
+            <div class="project-buttons">
+                <h3 class="proyecto-titulo">Proyecto Big Data</h3>
+                <a href="seccion1.pdf" class="button" target="_blank">Sección #1</a>
+                <a href="seccion2.pdf" class="button" target="_blank">Sección #2</a>
+                <a href="lab3.pdf" class="button" target="_blank">Sección #3</a>
+                <a href="lab4.pdf" class="button" target="_blank">Sección #4</a>
+            </div>
+        </div>
+    </div>
+
+    <footer>
+        <p>© 2024 Javier Cerejido Cortés</p>
+    </footer>
+</body>
+</html>
+
